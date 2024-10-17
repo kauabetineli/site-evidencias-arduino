c3f4567559afaa4b125b921a6b1ceacc0e819798
Kaua Betineli <kauaikoto@gmail.com>
2024-10-17 13:47:56 -0300

    commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function loadHTML(file, elementId) {
+    fetch(file)
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById(elementId).innerHTML = data;
+    })
+    .catch(error => {
+        console.error('Error loading HTML:', error);
+    });
+}
